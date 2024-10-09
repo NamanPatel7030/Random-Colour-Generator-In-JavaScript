17fcca7ca1ebbe8bc60f7a080885b6f784272f30
Naman Developer <namandeveloper7030@gmail.com>
2024-10-09 20:55:00 +0530

    Updated

--- random.js
+++ random.js
@@ -1,23 +1,17 @@
-// let changeBtn = document.querySelector('button');
-// let changeH1 = document.querySelector('h1');
-
-// changeBtn.addEventListener('click',function(){
-//     const r = Math.floor(Math.random()*255);
-//     const g = Math.floor(Math.random()*255);
-//     const b = Math.floor(Math.random()*255);
-//     const newColor = `rgb(${r},${g},${b})`
-//     changeH1.innerHTML = newColor;
-//     document.body.style.backgroundColor =newColor;
-// })
-
 let changeBtn = document.querySelector('button');
 let changeH1 = document.querySelector('h1');
 
-changeBtn.addEventListener('click', function() {
+const makeRandomColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
-    const newColor = `rgb(${r}, ${g}, ${b})`; // Added commas between r, g, b
-    changeH1.innerHTML = newColor;
+    return  `rgb(${r}, ${g}, ${b})`;
+    
+
+}
+changeBtn.addEventListener('click', function() {
+    const newColor = makeRandomColor();
     document.body.style.backgroundColor = newColor;
+    changeH1.innerHTML = newColor;
+
 });
