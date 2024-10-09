c4b2fa309461103920c77e68c9665559000863f0
Naman Developer <namandeveloper7030@gmail.com>
2024-10-09 20:29:26 +0530

    Commit

---
+++ random.js
@@ -0,0 +1,9 @@
+let changeBtn = document.querySelector('#btn');
+
+changeBtn.addEventListener(click,function(){
+    const r = Math.floor(Math.random()*255);
+    const g = Math.floor(Math.random()*255);
+    const b = Math.floor(Math.random()*255);
+    const newColor = `rgb(${r} ${g} ${b})`
+    document.body.style.backgroundColor =newColor;
+})
