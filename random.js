b1e46d91fbcd6872d46d8c4adad6e6f60d79cb5b
Naman Developer <namandeveloper7030@gmail.com>
2024-10-09 20:48:32 +0530

    Working

--- random.js
+++ random.js
@@ -1,9 +1,23 @@
-let changeBtn = document.querySelector('#btn');
+// let changeBtn = document.querySelector('button');
+// let changeH1 = document.querySelector('h1');
 
-changeBtn.addEventListener(click,function(){
-    const r = Math.floor(Math.random()*255);
-    const g = Math.floor(Math.random()*255);
-    const b = Math.floor(Math.random()*255);
-    const newColor = `rgb(${r} ${g} ${b})`
-    document.body.style.backgroundColor =newColor;
-})
+// changeBtn.addEventListener('click',function(){
+//     const r = Math.floor(Math.random()*255);
+//     const g = Math.floor(Math.random()*255);
+//     const b = Math.floor(Math.random()*255);
+//     const newColor = `rgb(${r},${g},${b})`
+//     changeH1.innerHTML = newColor;
+//     document.body.style.backgroundColor =newColor;
+// })
+
+let changeBtn = document.querySelector('button');
+let changeH1 = document.querySelector('h1');
+
+changeBtn.addEventListener('click', function() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const newColor = `rgb(${r}, ${g}, ${b})`; // Added commas between r, g, b
+    changeH1.innerHTML = newColor;
+    document.body.style.backgroundColor = newColor;
+});
